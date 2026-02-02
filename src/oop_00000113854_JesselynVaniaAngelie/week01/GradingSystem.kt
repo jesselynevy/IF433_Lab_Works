909f6f34060220443ce03a545912c225f6cbfde3
jesselyn <jesselynva@gmail.com>
2026-02-02 15:17:57 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000113854_JesselynVaniaAngelie/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_00000113854_JesselynVaniaAngelie.week01
+
+fun main(){
+    var name: String= "Jesselyn Vania Angelie"
+    var score: Int = 100
+
+    // Concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
