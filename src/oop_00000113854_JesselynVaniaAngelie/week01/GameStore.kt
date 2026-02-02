3477f1d21d89bc24f452edd46e9f98328db0458e
jesselyn <jesselynva@gmail.com>
2026-02-02 16:07:27 +0700

    week01: (tugas 2) setup GameStore file

---
+++ src/oop_00000113854_JesselynVaniaAngelie/week01/GameStore.kt
@@ -0,0 +1,8 @@
+package oop_00000113854_JesselynVaniaAngelie.week01
+
+fun main () {
+    val gameTitle: String = "Genshin Impact"
+    val price: Int = 67000
+
+    println("Game Title: $gameTitle, Price: $price")
+}
