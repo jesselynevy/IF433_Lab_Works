c9cfe75bba53bf5705a1eb6015d8804e503647cf
jesselyn <jesselynva@gmail.com>
2026-02-02 15:23:46 +0700

    week01: refactor to use val and string templates

--- src/oop_00000113854_JesselynVaniaAngelie/week01/GradingSystem.kt
+++ src/oop_00000113854_JesselynVaniaAngelie/week01/GradingSystem.kt
@@ -1,9 +1,10 @@
 package oop_00000113854_JesselynVaniaAngelie.week01
 
 fun main(){
-    var name: String= "Jesselyn Vania Angelie"
-    var score: Int = 100
+    // REFACTOR: Gunakan val dan hapus tipe data eksplisit
+    val name = "Jesselyn Vania Angelie"
+    val score = 100
 
-    // Concatenation gaya lama
-    println("Nama: " + name + ", Nilai: " + score)
+    // REFACTOR: String Template ($name)
+    println("Nama: $name, Nilai: $score")
 }
