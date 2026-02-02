efb1f8e633f7acc727d2661e4a0ada21da4396b7
jesselyn <jesselynva@gmail.com>
2026-02-02 16:03:31 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_00000113854_JesselynVaniaAngelie/week01/CircleCalculator.kt
+++ src/oop_00000113854_JesselynVaniaAngelie/week01/CircleCalculator.kt
@@ -1,14 +1,14 @@
 package oop_00000113854_JesselynVaniaAngelie.week01
 
-fun main(args: Array<String>) {
-    // Variable definition
+fun main() {
+
     val radius = 7.0
     val pi = 3.14
-    // Calculation
+
     val area = pi * radius * radius
-    // Output Concatenation
+
     println("Radius: $radius, Area: $area")
-    // Logic check
+
     println(checkSize(area))
 }
 fun checkSize(area: Double) = if (area > 100) "This is a Big Circle" else "This is a Small Circle"
