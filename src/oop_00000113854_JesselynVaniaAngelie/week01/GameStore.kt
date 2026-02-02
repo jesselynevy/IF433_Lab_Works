5cb67c50c75021856f76e8258f83ac045d1236cf
jesselyn <jesselynva@gmail.com>
2026-02-02 16:13:16 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000113854_JesselynVaniaAngelie/week01/GameStore.kt
+++ src/oop_00000113854_JesselynVaniaAngelie/week01/GameStore.kt
@@ -2,7 +2,22 @@ package oop_00000113854_JesselynVaniaAngelie.week01
 
 fun main () {
     val gameTitle: String = "Genshin Impact"
-    val price: Int = 67000
+    val price: Int = 670000
+    val finalPrice = calculateDiscount(price)
 
     println("Game Title: $gameTitle, Price: $price")
+    println("Original Price: Rp $price")
+    println("Final Price: Rp $finalPrice")
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) {
+        price - (price * 20 / 100)
+    } else {
+        price - (price * 10 / 100)
+    }
+    //if using "when"
+    //when {
+    //    price > 500_000 -> price - (price * 20 / 100)
+    //    else -> price - (price * 10 / 100)
+    //}
