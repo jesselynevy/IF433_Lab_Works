54eca73d83ecf675d2aa89e7d09d080c07655241
jesselyn <jesselynva@gmail.com>
2026-02-09 15:52:27 +0700

    week02: (task 1) implement library input system

--- src/oop_00000113854_JesselynVaniaAngelie/week02/Loan.kt
+++ src/oop_00000113854_JesselynVaniaAngelie/week02/Loan.kt
@@ -2,7 +2,7 @@ package oop_00000113854_JesselynVaniaAngelie.week02
 
 class Loan (
     val bookTitle: String,
-    var borrower: String,
+    val borrower: String,
     var loanDuration: Int = 1
 ){
     fun calculateFine() = if (loanDuration > 3) (loanDuration - 3) * 2000 else 0
--- src/oop_00000113854_JesselynVaniaAngelie/week02/Main.kt
+++ src/oop_00000113854_JesselynVaniaAngelie/week02/Main.kt
@@ -5,46 +5,65 @@ import java.util.Scanner
 fun main(){
     val scanner = Scanner(System.`in`)
 
-    println("--- APLIKASI PMB UMN ---")
+//    println("--- APLIKASI PMB UMN ---")
+//
+//    print("Masukkan Nama: ")
+//    val name = scanner.nextLine()
+//
+//    print("Masukkan NIM (Wajib 5 Karakter): ")
+//    val nim =scanner.next()
+//
+//    scanner.nextLine() // Bersihkan buffer newline (Penyakit klasik Scanner)
+//
+//    // Validasi di sisi pemanggil (Main)
+//    if (nim.length != 5){
+//        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
+//        // Program berhenti di sini untuk mahasiswa ini, tidak membuat objek
+//    } else {
+//        print("Masukkan Jurusan: ")
+//        val major = scanner.nextLine()
+//
+//        // Instansiasi Objek karena data sudah aman
+//        val s1 = Student(name, nim, major)
+//        println("${s1.name}, ${s1.nim}, ${s1.major}, ${s1.gpa}")
+//        println("Status: Pendaftaran Selesai.")
+//
+//        // ... After validation
+//        print("Pilih Jalur (1. Reguler, 2. Umum): ")
+//        val type = scanner.nextInt()
+//        scanner.nextLine() // Consume newLine
+//
+//        if (type == 1) {
+//            print("Masukkan Jurusan: ")
+//            val major = scanner.nextLine()
+//            // Memanggil Primary Constructor
+//            val s1 = Student(name, nim, major)
+//            println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+//        } else if (type == 2) {
+//            // Memanggil Secondary Constructor, jurusan otomatis "Non-Matriculated"
+//            val s2 = Student(name, nim)
+//            println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+//        } else {
+//            println("Pilihan tidak tepat, pendaftaran batal")
+//        }
+//    }
+    print("Masukkan Judul: ")
+    val bookTitle = scanner.nextLine()
 
-    print("Masukkan Nama: ")
-    val name = scanner.nextLine()
+    print("Masukkan Peminjam: ")
+    val borrower = scanner.nextLine()
 
-    print("Masukkan NIM (Wajib 5 Karakter): ")
-    val nim =scanner.next()
+    print("Masukkan Lama Pinjam (hari): ")
+    var loanDuration = scanner.nextInt()
 
-    scanner.nextLine() // Bersihkan buffer newline (Penyakit klasik Scanner)
-
-    // Validasi di sisi pemanggil (Main)
-    if (nim.length != 5){
-        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
-        // Program berhenti di sini untuk mahasiswa ini, tidak membuat objek
-    } else {
-        print("Masukkan Jurusan: ")
-        val major = scanner.nextLine()
-
-        // Instansiasi Objek karena data sudah aman
-        val s1 = Student(name, nim, major)
-        println("${s1.name}, ${s1.nim}, ${s1.major}, ${s1.gpa}")
-        println("Status: Pendaftaran Selesai.")
-
-        // ... After validation
-        print("Pilih Jalur (1. Reguler, 2. Umum): ")
-        val type = scanner.nextInt()
-        scanner.nextLine() // Consume newLine
-
-        if (type == 1) {
-            print("Masukkan Jurusan: ")
-            val major = scanner.nextLine()
-            // Memanggil Primary Constructor
-            val s1 = Student(name, nim, major)
-            println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
-        } else if (type == 2) {
-            // Memanggil Secondary Constructor, jurusan otomatis "Non-Matriculated"
-            val s2 = Student(name, nim)
-            println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
-        } else {
-            println("Pilihan tidak tepat, pendaftaran batal")
-        }
+    if (loanDuration < 0){
+        loanDuration = 1
     }
+
+    val loan1 = Loan(bookTitle, borrower, loanDuration)
+    println("\n=== Detail Peminjaman ===")
+    println("Judul Buku   : ${loan1.bookTitle}")
+    println("Peminjam    : ${loan1.borrower}")
+    println("Lama Pinjam : ${loan1.loanDuration} hari")
+    println("Total Denda : Rp ${loan1.calculateFine()}")
 }
