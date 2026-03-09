30e59d7f48b857c4e02b448fa88624a55c83931d
jesselyn <jesselynva@gmail.com>
2026-03-09 13:56:26 +0700

    week06: (task) create SmartLamp

---
+++ src/oop_00000113854_JesselynVaniaAngelie/week06/SmartLamp.kt
@@ -0,0 +1,10 @@
+package oop_00000113854_JesselynVaniaAngelie.week06
+
+class SmartLamp (override val id: String, override val name: String): SmartDevice, Switchable{
+    override fun turnOn() {
+        println("Lampu \$name berhasil dinyalakan.")
+    }
+    override fun turnOff() {
+        println("Lampu \$name berhasil dimatikan.")
+    }
+}
