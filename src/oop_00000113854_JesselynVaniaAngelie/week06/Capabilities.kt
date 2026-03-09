8d7df192d8d023c0ba01f805f6ffc4962b9b001c
jesselyn <jesselynva@gmail.com>
2026-03-09 13:04:53 +0700

    week06: create compound interfaces

---
+++ src/oop_00000113854_JesselynVaniaAngelie/week06/Capabilities.kt
@@ -0,0 +1,10 @@
+package oop_00000113854_JesselynVaniaAngelie.week06
+
+// Interface = Can-Do (Behavior)
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable {
+    fun chargeBattery()
+}
