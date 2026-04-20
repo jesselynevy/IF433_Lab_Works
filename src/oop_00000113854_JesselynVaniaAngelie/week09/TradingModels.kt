e6d286a853a69068f38339b44f2b70113f215733
jesselyn <jesselynva@gmail.com>
2026-04-20 14:46:36 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_00000113854_JesselynVaniaAngelie/week09/TradingModels.kt
@@ -0,0 +1,10 @@
+package oop_00000113854_JesselynVaniaAngelie.week09
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
+
