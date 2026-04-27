6293c0094cde7cf30d91d1415a76738402911293
jesselyn <jesselynva@gmail.com>
2026-04-27 14:02:16 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_00000113854_JesselynVaniaAngelie/week10/CryptoDashboard.kt
+++ src/oop_00000113854_JesselynVaniaAngelie/week10/CryptoDashboard.kt
@@ -6,4 +6,6 @@ fun main(){
     coinRepo.add(Coin("BTC", 167.0))
     coinRepo.add(Coin("ETH", 237.0))
     coinRepo.add(Coin("USDT", 180.0))
+
+    val response = ApiResponse("200 OK", coinRepo.getAll())
 }
