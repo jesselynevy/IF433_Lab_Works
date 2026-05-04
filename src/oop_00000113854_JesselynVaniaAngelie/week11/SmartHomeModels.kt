cf4201d2e1c11848aef2359b6e8d4cdeb693bda1
jesselyn <jesselynva@gmail.com>
2026-05-04 14:03:41 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_00000113854_JesselynVaniaAngelie/week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_00000113854_JesselynVaniaAngelie.week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
