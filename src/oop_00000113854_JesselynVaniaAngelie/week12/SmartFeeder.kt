85a3f796e7c0a10106a076d8e32ece802bb9d3cd
jesselyn <jesselynva@gmail.com>
2026-05-11 15:36:01 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_00000113854_JesselynVaniaAngelie/week12/SmartFeeder.kt
@@ -0,0 +1,7 @@
+package oop_00000113854_JesselynVaniaAngelie.week12
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0){
+        "Porsi kibble harus lebih dari 0 gr"
+    }
+}
