ad9bb86958eb2f41b2e6eeac84dea5c62f12b25e
jesselyn <jesselynva@gmail.com>
2026-05-18 13:32:09 +0700

    week13: create Student model

---
+++ src/oop_00000113854_JesselynVaniaAngelie/week13/StudentSerialization.kt
@@ -0,0 +1,9 @@
+package oop_00000113854_JesselynVaniaAngelie.week13
+import java.io.File
+import java.io.FileNotFoundException
+
+data class Student(
+    val name: String,
+    val age: Int,
+    val gpa: Double
+)
