40eabdc71337063925c53f12d2a7154aaf7cb84d
jesselyn <jesselynva@gmail.com>
2026-05-18 13:52:43 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_00000113854_JesselynVaniaAngelie/week13/CryptoTradeLogger.kt
@@ -0,0 +1,9 @@
+package oop_00000113854_JesselynVaniaAngelie.week13
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
